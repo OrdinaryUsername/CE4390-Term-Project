3f6f96f3be3ca56a423e946a7d6c11cf610d80b5
OrdinaryUsername <49770491+OrdinaryUsername@users.noreply.github.com>
2020-11-15 12:21:58 -0600

    Add files via upload

--- client.py
+++ client.py
@@ -1,8 +1,10 @@
 import socket
 import shutil
 
-def client_main(client = socket):
-    #initialize client cocket
+
+def client_main(client = server):
+    
+    #initialize client socket
     client.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     #set the client's target ip address(server ip address)
     client.target_ip = input('Enter target ip: ')
@@ -11,22 +13,34 @@ def client_main(client = socket):
     #connect the client to the server
     client.s.connect((client.target_ip,int(client.target_port)))
     
+    #accepted cmds for input
+    acceptedCmds = ['ld' , 'copy', 'rename', 'delete', 'done']
+    
     #listen
     while 1:
-        print 'Instruction List:'
-        print '"copy[filename]" retrieves a copy of a file from the server.'
-        print '"rename[filename, newFileName]" renames a file from the server.'
-        print '"delete[filename]" deletes a file on the server.'
-        print '"ld" lists file directory on server.'
-        print '"done" quits program.'
+        print ('Instruction List:')
+        print ('"ld" lists file directory on server.')
+        print ('"copy[filename]" retrieves a copy of a file from the server.')
+        print ('"rename[filename, newFileName]" renames a file from the server.')
+        print ('"delete[filename]" deletes a file on the server.')
+        print ('"done" quits program.')
         
         #get the user's command
         cmd = input('Enter a command: ')
-        if cmd == 'ld':
-            sl()
-            break
-        else if cmd == 'copy':
-            
+        #Split cmd for check
+        cmdCheck = cmd.split()
+        #Checks if user input matches 
+        if (cmdCheck[0] in acceptedCmds):
+            if (cmd == 'done'):
+                socket.send(cmd.encode());
+                break
+            else:
+                client.s.send(cmd.encode())
+                data = client.s.recv(1024)
+                print (data.decode())
+        else:
+            print('Please enter valid command!')
+        
         #Convert command to protocol format for client_message
         #send command to server using client.s.send(client_message.encode())
         #recieve message from server using server_message = client.s.recv(1024)
@@ -42,7 +56,7 @@ def client_main(client = socket):
         #open the socket
         client.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         #connect to the server
-        client.s.connect((client.target_ip,int(client.target_port)))
+        client.s.connect((client.target_ip, int(client.target_port)))
 
     #shut down the connection
     client.s.shutdown(socket.SHUT_RDWR)
