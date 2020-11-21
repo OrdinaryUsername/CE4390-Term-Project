aef2e46297a505608234b0f23c71e1c71bdaa363
OrdinaryUsername <49770491+OrdinaryUsername@users.noreply.github.com>
2020-11-21 11:12:26 -0600

    Add files via upload

--- client.py
+++ client.py
@@ -2,76 +2,64 @@ import socket
 import shutil
 
 
-def client_main(client=socket):
+def client_main():
     # initialize client socket
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # set the client's target ip address(server ip address)
-    # client.target_ip = input('Enter target ip: ')
-    #target_ip = input('Enter target ip: ')
+
     target_ip = '10.0.0.1'
-    #target_ip = socket.gethostbyname("mininet-vm")
-    # set the client's target port(server port)
-    # client.target_port = input('Enter target port: ')
-    #target_port = int(input('Enter target port: '))
+    
     target_port = 49159
-    # connect the client to the server
-    # client.s.connect((client.target_ip,int(client.target_port)))
-    client.connect((target_ip, target_port))
-    print("connected")
+ 
     # accepted cmds for input
-    acceptedCmds = ['ld', 'copy', 'rename', 'delete', 'done']
+    acceptedCmds = ['list', 'copy', 'rename', 'delete', 'done']
 
     # listen
     while 1:
+        client.connect((target_ip, target_port))
+        print(' ')
+        print("Connected to server")
+        print(' ')
+        print('------------------------------------------------------------------')
         print('Instruction List:')
-        print('"ld" lists file directory on server.')
+        print('"list" lists file directory on server.')
         print('"copy[filename]" retrieves a copy of a file from the server.')
         print('"rename[filename, newFileName]" renames a file from the server.')
         print('"delete[filename]" deletes a file on the server.')
         print('"done" quits program.')
-
+        print('------------------------------------------------------------------')
+        print(' ')
         # get the user's command
         cmd = input('Enter a command: ')
+        print(' ')
         # Split cmd for check
-        cmdCheck = cmd.split()
+        cmdCheck = cmd.split(' ')
         # Checks if user input matches
         if (cmdCheck[0] in acceptedCmds):
             if (cmd == 'done'):
-                socket.send(cmd.encode());
+                client.send(bytes(cmd, "utf-8"))
                 break
             else:
-                client.s.send(cmd.encode())
-                data = client.s.recv(1024)
-                print(data.decode())
+                client.send(bytes(cmd, "utf-8"))
+                data = client.recv(1024)
+                print(data.decode("utf-8"))
         else:
             print('Please enter valid command!')
 
-        # Convert command to protocol format for client_message
-        # send command to server using client.s.send(client_message.encode())
-        # recieve message from server using server_message = client.s.recv(1024)
-        #   Note: may need to use while 1 loop to get all of the message contents
-        #   and break when no longer recieving anything
-        # decode server message using server_message.decode()
-        #   Note: check out shutil library for file copying??
-
+    
         # shut down the connection
-        client.s.shutdown(socket.SHUT_RDWR)
+        client.shutdown(socket.SHUT_RDWR)
         # close the socket
-        client.s.close()
+        client.close()
         # open the socket
-        client.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # connect to the server
-        #client.s.connect((client.target_ip, int(client.target_port)))
-        client.connect((target_ip, target_port))
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # shut down the connection
-    client.s.shutdown(socket.SHUT_RDWR)
+    client.shutdown(socket.SHUT_RDWR)
     print("shutdown")
     # close the socket
-    client.s.close()
+    client.close()
     print("close")
 
 
 # use client_main() as the main function
-if __name__ == "__main__":
-    client_main()
+if __name__ == "__main__": client_main()
--- server.py
+++ server.py
@@ -6,105 +6,96 @@ import shutil
 
 
 def server_main(server=socket):
+    print('------------------------------------------------------------------')
     # create and initialize server socket
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # get the server's ip address
     # ip = socket.gethostbyname(socket.gethostname())
     #host = socket.gethostbyname(socket.gethostname())
     host = '10.0.0.1'
-    print(socket.gethostname())
+    print('Server Host name: ' + socket.gethostname())
     # ip = input("Enter IP Address: ")
     # get the port number for the host
     #port = int(input('Enter desired port: '))
     port = 49159
     # bind the socket using the ip address and port number
     server.bind((host, port))
-    print("bound")
     # listen for connection requests
     server.listen(1000000)
 
-    print('Listening on IP: ' + host + ', ' + str(port))
+    print('Listening on IP: ' + host + ', ' + str(port) + '\n')
     # accept the connection request
-    server.accept()
-    print("Connection accepted")
 
     while 1:
-
-
+        conn, addr = server.accept()
+        print("Connection accepted")
         # recieve message from client using client_message = server.recv(1024)
-        data = server.recv(1024)
-        print("recieving data")
-        # Add the data to the client message
-        client_message = data
-        data = server.recv(1024)
-        # while there is still data coming in
-        #   Note: may need to use while 1 loop to get all of the message contents
-        #   and break when no longer recieving anything
-        while data:
-            # add the data to the client message
-            client_message += data
-            # recieve the data
-            data = server.recv(1024)
-
-        print("decoding data")
+        data = conn.recv(1024)
+        print("Recieving data....")
         # decode server message using client_message.decode()
-        client_message.decode()
-
-        # split the client message to get the command type and parameters
-        # in separate strings in an array of strings
-        cmd = client_message.split(" ")
-        print(cmd)
-
+        cmd = data.decode("utf-8")
+        print("Decoding data....")
+        #cmd = data.split(' ')
+        #print(cmd[0])
+        if cmd == 'list':
+            print('Client requesting file directory...')
+            server_message = ld()
+            print('Sending directory....')
+            conn.send(bytes(str(server_message), "utf-8"))
         # do stuff based on the message contents and protocol format (flags, etc)
         #   Note: look at os library for file management functions??
         #   Note: look at shutil library for file copying??
-        if cmd[0] == 'copy':
+        userInput = cmd.split(' ')
+        
+        if userInput[0] == 'copy':
             # maybe append current directory onto front of filenames
-            server_message = copy(cmd[1])
+            print('Attempting to copy file' + userInput[1] + '...')
+            server_message = copy(userInput[1])
             print(server_message)
-            server.send(server_message.encode())
-        elif cmd[0] == 'rename':
+            conn.send(bytes(str(server_message), "utf-8"))
+        elif userInput[0] == 'rename':
+            print('Attempting to rename file' + userInput[1] + 'to ' + userInput[2])
             # maybe append current directory onto front of filenames
-            server_message = rename(cmd[1], cmd[2])
+            server_message = rename(userInput[1], userInput[2])
             print(server_message)
-            server.send(server_message.encode())
-        elif cmd[0] == 'delete':
+            conn.send(bytes(str(server_message), "utf-8"))
+        elif userInput[0] == 'delete':
+            print('Attempting to delete file' + userInput[1] + '...')
             # maybe append current directory onto front of filenames
-            server_message = delete(cmd[1])
+            server_message = delete(userInput[1])
             print(server_message)
-            server.send(server_message.encode())
-        elif cmd[0] == 'ld':
-            server_message = ld()
-            print(server_message)
-            server.send(server_message.encode())
-        elif cmd[0] == 'done':
+            conn.send(bytes(server_message, "utf-8"))
+        elif userInput[0] == 'done':
+            print('Client has requested to close connection...')
             server_message = 'done'
-            print(server_message)
-            server.send(server_message.encode())
+            #print(server_message)
+            conn.send(bytes(server_message, "utf-8"))
             # shutdown connection with client
-            server.shutdown(socket.SHUT_RDWR)
+            print('Closing connection with client')
+            conn.shutdown(socket.SHUT_RDWR)
             # close server socket
-            server.close()
+            print('Shutting down server...')
+            conn.close()
             break
 
         # shutdown connection with client
-        server.shutdown(socket.SHUT_RDWR)
-        print("shutdown")
+        conn.shutdown(socket.SHUT_RDWR)
+        print("Closing connection with client...")
         # close server socket
-        server.close()
-        print("close")
+        #conn.close()
+        print("Re-establising connection")
 
 
 # Function to copy a file
-def copy(filename=string):
+def copy(filename):
     currWorkingDir = os.getcwd()
     fullpath = currWorkingDir + "/" + filename
-    copyfullpath = "COPY" + fullpath
-
-    # do stuff for copying a file
-
-    # return message about success
-    if path.exists('copyfullpath'):
+    newfileName = filename + '(1)'
+    newLocation = currWorkingDir + "/" + newfileName
+    shutil.copy2(fullpath, newLocation)
+    copyfullpath = newLocation
+    print(fullpath)
+    if path.exists(copyfullpath):
         return "File Copy Success"
     else:
         return "Error: File Copy Failed"
@@ -112,7 +103,7 @@ def copy(filename=string):
 
 
 # Function to rename a file
-def rename(filename1=string, filename2=string):
+def rename(filename1, filename2):
     currWorkingDir = os.getcwd()
     fullpath1 = currWorkingDir + "/" + filename1
     fullpath2 = currWorkingDir + "/" + filename2
@@ -120,10 +111,10 @@ def rename(filename1=string, filename2=string):
     print(fullpath2)
 
     #rename file
-    os.rename(r'fullpath1', r'fullpath2')
+    os.rename(fullpath1, fullpath2)
 
     #check for existence of new filename
-    if path.exists('fullpath2'):
+    if path.exists(fullpath2):
         return "File Rename Success"
     else:
         return "Error: File Rename Failed"
@@ -131,29 +122,24 @@ def rename(filename1=string, filename2=string):
 
 
 # Function to delete a file
-def delete(filename=string):
+def delete(filename):
     currWorkingDir = os.getcwd()
     fullpath = currWorkingDir + "/" + filename
     print(fullpath)
-
-    # do stuff to delete the file
-
-    # return message about success
-    #check for existence of new filename
-    if path.exists('fullpath'):
-        return "Error: File Rename Failed"
+    if os.path.exists(fullpath):
+      os.remove(fullpath)
+      return "File delete success"
     else:
-        return "File Delete Success"
+      return "The file does not exist"
+      
+    
 
 
 # Function to get the list of files from the directory
 def ld():
     currWorkingDir = os.getcwd()
-    print("FIXME")
-    # Do stuff to send the list from directory
-    # return string of filenames in the directory
-    # or message about success
-    return "FIXME"
+    directory = os.listdir(currWorkingDir)
+    return directory
 
 
 # use server_main() as the main function
